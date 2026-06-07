4314310ce99d60caa49c529878c18532080533e6
Mallinath-cs <mallinath.cshakapure@gmail.com>
2026-06-07 22:05:51 +0530

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        return modifiedBinarySearch(nums, target, left, right);
+    }
+    public int modifiedBinarySearch(int[] nums, int target, int left, int right){
+        if( left > right){
+            return -1;
+        }
+        int mid = left + (right - left) / 2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid] >= nums[left]){
+
+            if(nums[left] <= target && target <= nums[mid]){
+                return modifiedBinarySearch(nums, target, left, mid -1);
+            }
+            else{
+                return modifiedBinarySearch(nums, target, mid + 1, right);
+            }
+        }
+        else{
+            if(nums[mid] <= target && target <= nums[right]){
+                return modifiedBinarySearch(nums, target, mid + 1, right);
+            }
+            else{
+                return modifiedBinarySearch(nums, target, left, mid -1);
+            }
+        }
+    }
+}
